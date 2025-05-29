9eac7cc6d31179f6a08629af74a6f9d7e3f8a7eb
DANTE urso <osrudante@gmail.com>
2025-05-29 18:33:32 -0400

    fix user update controller

--- test/api/user/update-user-test.js
+++ test/api/user/update-user-test.js
@@ -1,63 +1,49 @@
-let should;
-let agent;
-let mockData;
+let should
+let agent
+let mockData
 
 before(() => {
-  should = require('should');
-  agent = require('test/lib/agent');
-  mockData = require('test/lib/mock-data');
-});
+  should = require('should')
+  agent = require('test/lib/agent')
+  mockData = require('test/lib/mock-data')
+})
 
 describe('api', () => {
   describe('user', () => {
-    describe('update permissions', () => {
-      let userA, userB;
-      let authA, authB;
+    describe('PUT /dev/user/:id authorization', () => {
+      let authA
+      let authB
 
       before(async () => {
         // Create User A
-        const emailA = `${mockData.uuid()}@test.com`;
-        const passwordA = mockData.uuid();
-        userA = await mockData.mockAuthAndUser({
-          email: emailA,
-          password: await mockData.hash(passwordA),
-        });
-        authA = await agent.client().post('/auth/login').send({ email: emailA, password: passwordA }).expect(200).promise();
-
+        authA = await mockData.mockAuthAndUser()
         // Create User B
-        const emailB = `${mockData.uuid()}@test.com`;
-        const passwordB = mockData.uuid();
-        userB = await mockData.mockAuthAndUser({
-          email: emailB,
-          password: await mockData.hash(passwordB),
-        });
-        authB = await agent.client().post('/auth/login').send({ email: emailB, password: passwordB }).expect(200).promise();
-      });
+        authB = await mockData.mockAuthAndUser()
+      })
 
-      it('should not allow userA to update userB\'s data', async () => {
-        const update = { email: 'unauthorized@attempt.com' };
-
+      it('should not allow userA to update userB', async () => {
         await agent
-          .client(authA.auth)
-          .put(`/dev/user/${authB.user.id}`)
-          .send(update)
+          .client()
+          .put(`/user/${authB.user}`)
+          .set('authorization', authA.token)
+          .send({ email: 'unauthorized@hax.com' })
           .expect(403)
-          .promise();
-      });
+          .promise()
+      })
 
       it('should allow userB to update their own data', async () => {
-        const update = { email: 'updated_b@example.com' };
-
-        const { body: updatedUser } = await agent
-          .client(authB.auth)
-          .put(`/dev/user/${authB.user.id}`)
-          .send(update)
+        const newEmail = 'authorized@hax.com';
+        const res = await agent
+          .client()
+          .put(`/user/${authB.user}`)
+          .set('authorization', authB.token)
+          .send({ email: 'authorized@hax.com' })
           .expect(200)
-          .promise();
+          .promise()
 
-        should.exist(updatedUser);
-        updatedUser.email.should.equal(update.email);
-      });
-    });
-  });
-});
+        should.exist(res)
+        res.email.should.equal(newEmail)
+      })
+    })
+  })
+})
--- test/lib/mock-data.js
+++ test/lib/mock-data.js
@@ -1,48 +1,61 @@
-let should
-let agent
-let mockData
+const bcrypt = require('bcryptjs')
+const { v4: uuid } = require('uuid')
+const authService = require('app/modules/auth')
+const userService = require('app/modules/user')
 
-before(() => {
-  should = require('should')
-  agent = require('test/lib/agent')
-  mockData = require('test/lib/mock-data')
-})
+class MockData {
+  /**
+   * @method uuid
+   */
+  uuid() {
+    return uuid(...arguments)
+  }
 
-describe('api', () => {
-  describe('user', () => {
-    describe('PUT /dev/user/:id authorization', () => {
-      let authA
-      let authB
+  /**
+   * @method hash
+   */
+  hash(input) {
+    return bcrypt.hash(input, authService.SALT_WORK_FACTOR)
+  }
 
-      before(async () => {
-        // Create User A
-        authA = await mockData.mockAuthAndUser()
-        // Create User B
-        authB = await mockData.mockAuthAndUser()
-      })
+  /**
+   * @method mockAuthAndUser
+   */
+  async mockAuthAndUser(options = {}) {
+    const user = await this.mockUser(options)
+    const auth = await this.mockAuth({ ...options, user: user.id })
+    return auth
+  }
 
-      it('should not allow userA to update userB', async () => {
-        await agent
-          .client(authA)
-          .put(`/dev/user/${authB.user}`)
-          .send({ email: 'unauthorized@hax.com' })
-          .expect(403)
-          .promise()
-      })
+  /**
+   * @method mockAuth
+   */
+  mockAuth(options = {}) {
+    const data = Object.assign(
+      {
+        token: uuid(),
+        user: uuid(),
+        password: uuid()
+      },
+      options
+    )
+    return authService.create(data)
+  }
 
-      it('should allow userB to update their own data', async () => {
-        const newEmail = 'legit@update.com'
+  /**
+   * @method mockUser
+   */
+  mockUser(options = {}) {
+    const data = Object.assign(
+      {
+        email: `${uuid()}@test.com`,
+        firstName: 'John',
+        lastName: 'Doe'
+      },
+      options
+    )
+    return userService.create(data)
+  }
+}
 
-        const res = await agent
-          .client(authB)
-          .put(`/dev/user/${authB.user}`)
-          .send({ email: newEmail })
-          .expect(200)
-          .promise()
-
-        should.exist(res)
-        res.email.should.equal(newEmail)
-      })
-    })
-  })
-})
+module.exports = new MockData()
